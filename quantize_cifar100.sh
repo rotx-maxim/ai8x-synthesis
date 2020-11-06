b341172ce39ab43c71f5145d1c92444a930bc4ba
Gorkem Ulkar <Gorkem.Ulkar@maximintegrated.com>
2020-11-06 19:27:21 +0300

    Default scale changed in quantize.py, new models with QAT8 added

--- quantize_cifar100.sh
+++ quantize_cifar100.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./quantize.py trained/ai85-cifar100-noqat.pth.tar trained/ai85-cifar100-noqat-q.pth.tar --device MAX78000 -v -c networks/cifar100-simple.yaml --scale 1.0 "$@"
+./quantize.py trained/ai85-cifar100-qat8.pth.tar trained/ai85-cifar100-qat8-q.pth.tar --device MAX78000 -v "$@"
